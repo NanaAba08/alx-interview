177a1cdb1dd290ae15ef89172642ca834a89f45d
NanaAba08 <williamsnanaaba@gmail.com>
2024-02-06 14:43:03 -0800

    adding shebang

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
 def pascal_triangle(n):
     """Create a function def pascal_triangle(n): that returns a list of lists
     of integers representing the Pascal’s triangle of n
