4d167f66d76505a461f69d5a70471e2e1af09f98
NanaAba08 <williamsnanaaba@gmail.com>
2024-02-14 13:16:51 -0800

    unlock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    visited = [False] * n
+    stack = [0]  # Start with box 0, which is unlocked by default
+    visited[0] = True
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if not visited[key]:
+                stack.append(key)
+                visited[key] = True
+
+    return all(visited)
